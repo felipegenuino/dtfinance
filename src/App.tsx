a7b13ff3ffb0e873428f9a63825db21b14b7b05e
Felipe Genuino <design@felipegenuino.com>
2021-04-05 15:00:54 -0300

    Configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import Header from './components/Header'
 import Dashboard from './components/Dashboard'
 import { GlobalStyle } from './styles/globals';
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+  // function afterOpenModal() {
+  //     console.log('fechou modal')
+  // }
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onAfterOpen={handleOpenNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+        style={customStyles}
+        contentLabel="Cadastrar transação"
+      >
+
+        <h2>Cadastrar transação</h2>
+        <button onClick={handleCloseNewTransactionModal}>close</button>
+        <div>I am a modal</div>
+
+      </Modal>
       <GlobalStyle />
     </>
   );
